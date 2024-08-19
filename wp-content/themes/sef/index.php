00b320f19b3cfe7f5a620eb55350f91b5043b0fb
benjamin <benjamin.marissiaux@student.hepl.be>
2024-08-19 16:45:44 +0200

    - hissue with the navigation mediaquery. - home page finished

--- wp-content/themes/sef/index.php
+++ wp-content/themes/sef/index.php
@@ -14,8 +14,6 @@ get_header();
 
 <main>
     <section class="intro wrapper">
-
-
         <div>
             <h1>Service d'<span class="mot-orange">Entraide</span> Familiale</h1>
             <p>
@@ -77,7 +75,7 @@ get_header();
             Les <span class="mot-orange">news</span> les plus <span class="mot-orange">récentes</span>
         </h1>
         <div class="cartes-container">
-            <article class="cartes-container__carte">
+            <article class="cartes-container__carte carte1">
                 <h2>
                     <?= get_field("carte-titre", 12) ?>
                 </h2>
@@ -89,7 +87,7 @@ get_header();
                 </p>
             </article>
 
-            <article class="cartes-container__carte">
+            <article class="cartes-container__carte carte2">
                 <h2>
                     <?= get_field("carte-titre-2", 12) ?>
                 </h2>
@@ -100,7 +98,7 @@ get_header();
                     <?= get_field("carte-texte-2", 12) ?>
                 </p>
             </article>
-            <article class="cartes-container__carte">
+            <article class="cartes-container__carte carte3">
                 <h2>
                     <?= get_field("carte-titre-3", 12) ?>
                 </h2>
@@ -111,6 +109,33 @@ get_header();
                     <?= get_field("carte-texte-3", 12) ?>
                 </p>
             </article>
+            <article class="cartes-container__carte carte4">
+                <h2>
+                    <?= get_field("carte-titre-4", 12) ?>
+                </h2>
+                <p>
+                    <?= get_field("date-4", 12) ?>
+                </p>
+                <p>
+                    <?= get_field("carte-texte-4", 12) ?>
+                </p>
+            </article>
+            <aside class="cartes-container__don">
+                <img src="http://sef-aout.test/wp-content/uploads/2024/08/carte-don.svg"
+                     alt="image d'un point d'interrogation entouré d'un cercle">
+                <h2>
+                    Soutenez&nbsp;-&nbsp;nous d&lsquo;une autre mani&egrave;re
+                </h2>
+                <a href="#" class="btn-orange">Faire un don</a>
+            </aside>
         </div>
     </section>
+    <section class="benevolat wrapper">
+        <h1>
+            Envie de devenir <span class="mot-orange">b&eacute;n&eacute;vole</span>&nbsp;?
+        </h1>
+        <a href="#">Contactez&nbsp;-&nbsp;nous</a>
+    </section>
 </main>
+
+<?php get_footer() ?>
